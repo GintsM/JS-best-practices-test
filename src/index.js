9a5518cb13c72139c5d50904377d460a53bd9ef0
Gints Misins <88575586+GintsM@users.noreply.github.com>
2021-11-08 14:34:13 +0200

    ToDo project for peer review

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import './style.css';
+import Tasks from './tasks.js';
+import Build from './ui.js';
+
+const addList = document.getElementById('addList');
+Build.addToPage(addList);
+
+const addTask = document.getElementById('move');
+addTask.addEventListener('click', () => {
+  const taskToAdd = document.getElementById('addTask');
+  const newTask = new Tasks(taskToAdd.value, false, Build.lastElementsIndex(addList));
+  Build.drawHtmlElement(newTask, addList);
+  Build.addTaskToLocalStore(newTask);
+  taskToAdd.value = '';
+});
+
+addList.addEventListener('click', (e) => {
+  if (e.target.hasAttribute('for')) {
+    Build.editTask(e);
+  } else if (e.target.className === 'hello') {
+    Build.completeTaskLocal(e);
+  } else if (e.target.parentNode.className === 'vdots') {
+    const hidenTrash = e.target.parentNode.nextElementSibling;
+    e.target.parentNode.classList.add('hide');
+    hidenTrash.classList.remove('hide');
+  } else if (e.target.parentNode.className === 'tras') {
+    const index = parseInt(e.target.id, 10) - 1;
+    Build.removeTask(e.target, index);
+  }
+});
+
+const clearDone = document.querySelector('body > div:nth-child(4) > h4');
+clearDone.addEventListener('click', () => {
+  Build.clearAllComplete();
+});
